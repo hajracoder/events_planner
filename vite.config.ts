3b82a65a2d8fd6b89ce493cae1b65d4697f90b08
hajracoder <hajraqadir94@gmail.com>
2025-08-26 11:33:35 -0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/events_planner",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // <-- ye add kiya
