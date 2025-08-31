1e554e8153f426613110652a07c95d26bd7cf094
hajracoder <hajraqadir94@gmail.com>
2025-08-31 03:08:04 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,36 +32,6 @@
 
 
 
-// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-// import Sidebar from "./components/Sidebar";
-// import Dashboard from "./components/Dashboard";
-// import Events from "./components/Events";
-// import Locations from "./components/Locations";
-// import Users from "./components/Users";
-
-// export default function App() {
-//   return (
-//     <Router basename="/events_planner/">
-//       <div className="flex">
-//         <Sidebar />
-//         <main className="flex-1 p-6">
-//           <Routes>
-//             <Route path="/" element={<Navigate to="/dashboard" replace />} />
-//             <Route path="/dashboard" element={<Dashboard />} />
-//             <Route path="/events" element={<Events />} />
-//             <Route path="/locations" element={<Locations />} />
-//             <Route path="/users" element={<Users />} />
-//           </Routes>
-//         </main>
-//       </div>
-//     </Router>
-//   );
-// }
-
-
-
-
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/Dashboard";
@@ -70,11 +40,8 @@ import Locations from "./components/Locations";
 import Users from "./components/Users";
 
 export default function App() {
-  // Detect environment: use basename only in production
-  const basename = import.meta.env.PROD ? "/events_planner/" : "";
-
   return (
-    <Router basename={basename}>
+    <Router basename="/events_planner">
       <div className="flex">
         <Sidebar />
         <main className="flex-1 p-6">
@@ -90,3 +57,7 @@ export default function App() {
     </Router>
   );
 }
+
+
+
+
