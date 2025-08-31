96909fe8c643c6944bab6b17e6c5223ca63edf51
hajracoder <hajraqadir94@gmail.com>
2025-08-31 03:08:21 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-base: process.env.VITE_BASE_PATH || "/events_planner/",
+base: process.env.VITE_BASE_PATH || "/events_planner",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
