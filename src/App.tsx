3f0238f259784f9ae1a56b94f549920555a54130
hajracoder <hajraqadir94@gmail.com>
2025-08-31 03:18:23 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,6 @@
 
 
 
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/Dashboard";
@@ -39,25 +38,30 @@ import Events from "./components/Events";
 import Locations from "./components/Locations";
 import Users from "./components/Users";
 
+const basename = import.meta.env.PROD ? "/events_planner" : "/";
+
 export default function App() {
   return (
-    <Router basename="/events_planner">
+    <Router basename={basename}>
       <div className="flex">
         <Sidebar />
-        <main className="flex-1 p-6">
+        <main className="flex-1 p-4">
           <Routes>
+            {/* Redirect root to dashboard */}
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
+            {/* Main routes */}
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/events" element={<Events />} />
             <Route path="/locations" element={<Locations />} />
             <Route path="/users" element={<Users />} />
+
+            {/* Catch all: redirect unknown paths */}
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
           </Routes>
         </main>
       </div>
     </Router>
   );
 }
-
-
-
 
